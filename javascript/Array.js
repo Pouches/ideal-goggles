e35407157b0fee65caee3ae16121b2ce9c323f4b
Andy <agdl2003@gmail>
2020-02-26 18:31:12 -0700

    Added javascript
    The arrays have started to be built and some css was changed so that it could better match the desired end

---
+++ javascript/Array.js
@@ -0,0 +1,25 @@
+//this first array would go to all td tags that have the id of a 
+var verticalrow1=[x]
+//this goes to the b id's and etc.
+/* 
+ the following is just an example of how i build the array but will not work with this one 
+
+ function setvalue(x){
+                if(check==true){ 
+                     document.getElementById("caldis").innerHTML+=x;
+                      num1=  document.getElementById("caldis").innerHTML;
+
+        
+*/
+var verticalrow2=[x]
+var verticalrow3=[x]
+var verticalrow4=[x]
+var verticalrow5=[x]
+var verticalrow6=[x]
+var verticalrow7=[x]
+var start = 1
+var cellcolor
+c4[0]=""
+c4[1]=""
+
+//create a functio that checks wheter the it has benn clicked before 
