81e5ae3b1563ea2563205eb2eda29cc843a06beb
Unknown <cadebrown1776@gmail.com>
2020-02-27 18:11:07 -0700

    JS work
    created a function to reset the position of the mouse when you mouseout of a table cell, W.I.P. a function that detects if  a space has already been chosen, function to write which player is playing, created a start button function.

---
+++ javascript/cade.js
@@ -0,0 +1,24 @@
+var colorchecker=undefined//color checker will check what turn it is
+var clickposition = undefined;//clickposition identifies where the puck should be placed
+//cheatchecker checks if a space has already been chose and alert the person who chose it
+
+function startbutton(){
+    colorchecker=true;
+}
+
+function playerbox(){
+if(colorchecker=true){
+    document.body.getElementById("playerindicator").innerHTML="Player: Cyan";
+    console.log("Player:Cyan")
+}
+}
+function mousepositionreset(){
+    clickposition=undefined;
+    console.log("variable called clickposition reset");
+}
+
+function cheatchecker(){
+    if(){
+        alert("Invalid Placement, Please try again");
+    }
+}
